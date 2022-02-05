688d1128e98e77130b6381dadbeadc17a006bdc9
Everton <everton@devgrid.co.uk>
2022-02-05 10:28:15 -0300

    Initial Commit

---
+++ CustomDropdownMenu/CustomDropdownMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomDropdownMenuApp.swift
+//  CustomDropdownMenu
+//
+//  Created by Everton Carneiro on 05/02/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomDropdownMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
